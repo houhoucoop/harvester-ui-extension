b7142497f910cd07b4abcf6ef7925b62f5dc084a
Yi-Ya Chen <yiya.chen@suse.com>
2025-02-13 16:03:58 +0800

    feat: v1.0.5

--- pkg/harvester/models/harvester/schema.js
+++ pkg/harvester/models/harvester/schema.js
@@ -3,12 +3,4 @@ import SteveSchema from '@shell/models/steve-schema';
 /**
  * This is the steve schema that's used for the harvester store
  */
-export default class HarvesterStoreSchema extends SteveSchema {
-  constructor(...args) {
-    if (args[0].type === 'schema') {
-      // eslint-disable-next-line no-debugger
-      debugger;
-    }
-    super(...args);
-  }
-}
+export default class HarvesterStoreSchema extends SteveSchema {}
