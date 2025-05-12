8410f758592221e4ad2bed254a142d82a0b506b5
Andy Lee <andy.lee@suse.com>
2025-05-12 10:48:01 +0800

    Remove unneeded condition (#279)
    Signed-off-by: Andy Lee <andy.lee@suse.com>

--- pkg/harvester/edit/kubevirt.io.virtualmachine/VirtualMachineSSHKey.vue
+++ pkg/harvester/edit/kubevirt.io.virtualmachine/VirtualMachineSSHKey.vue
@@ -6,7 +6,7 @@ import { LabeledInput } from '@components/Form/LabeledInput';
 import LabeledSelect from '@shell/components/form/LabeledSelect';
 import ModalWithCard from '@shell/components/ModalWithCard';
 
-import { _VIEW, _EDIT } from '@shell/config/query-params';
+import { _VIEW } from '@shell/config/query-params';
 
 import { NAMESPACE } from '@shell/config/types';
 import { HCI } from '../../types';
@@ -90,9 +90,6 @@ export default {
     },
 
     sshOption() {
-      if (this.mode === _VIEW || this.mode === _EDIT) {
-        return [];
-      }
       const out = this.$store.getters['harvester/all'](HCI.SSH).map( (O) => {
         return {
           label: O.id,
